69f78ec7717c2e2ee2bfde160c6f01010ed87898
Sandeep M <sandeep.manvar@outlook.com>
2021-05-13 23:48:04 +0530

    Made views functional with static files & templates
    - Added new templates with static files for views
    - Created following templates:
        - all-posts.html
        - post.html
        - post-detail.html
    - Configured URLs & make navigation functional
    - Updated base.html template
    - Updated settings

--- blog/views.py
+++ blog/views.py
@@ -8,8 +8,8 @@ def starting_page(request):
 
 
 def posts(request):
-    pass
+    return render(request, "blog/all-posts.html")
 
 
-def post_detail(request):
-    pass
+def post_detail(request, slug):
+    return render(request, "blog/post-detail.html")
