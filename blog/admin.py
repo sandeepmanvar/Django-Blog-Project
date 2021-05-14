cc5bc818beb106afdcc188ae34ea4d51f50cd97f
Sandeep M <sandeep.manvar@outlook.com>
2021-05-14 23:02:33 +0530

    Created models, register them to admin and migrate

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Author, Post, Tag
 
 # Register your models here.
+
+admin.site.register(Author)
+admin.site.register(Post)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=255)
+    image = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=CASCADE, related_name="posts")
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
