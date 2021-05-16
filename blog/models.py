738b7def3b2f161517bc4beb5f9f636eaac9131e
Sandeep M <sandeep.manvar@outlook.com>
2021-05-16 09:32:41 +0530

    Image upload functionality in adminpanel
    - add ImageField to the post model and update migrations
    - configure media root and url in setttings
    - remove static images from app
    - update global urls

--- blog/models.py
+++ blog/models.py
@@ -23,7 +23,7 @@ class Author(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=255)
-    image = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateTimeField(auto_now=True)
     slug = models.SlugField(unique=True)
     content = models.TextField()
